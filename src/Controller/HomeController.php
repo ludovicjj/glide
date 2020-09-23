04b789d36b4543e7ce5cebc7fdd978a8a2265559
ludovicjj <jahanlud@gmail.com>
2020-09-23 09:39:13 +0200

    home route

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+class HomeController
+{
+    /** @var Environment $twig */
+    private $twig;
+
+    public function __construct
+    (
+        Environment $twig
+    )
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function index()
+    {
+        $content = 'Hello world';
+
+        return new Response(
+            $this->twig->render(
+                'app/home.html.twig',
+                [
+                    'content' => $content
+                ]
+            )
+        );
+    }
+}
