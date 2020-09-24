d42da54b89465ad36a1f37a96e495b6ab043056c
ludovicjj <jahanlud@gmail.com>
2020-09-24 11:25:19 +0200

    Add validation signature into ImageController

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
-use App\Helper\StringHelper;
 use League\Glide\Filesystem\FileNotFoundException;
 use League\Glide\Responses\SymfonyResponseFactory;
 use League\Glide\ServerFactory;
+use League\Glide\Signatures\SignatureException;
+use League\Glide\Signatures\SignatureFactory;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -20,20 +21,30 @@ class ImageController
     /** @var string $cache */
     private $cache;
 
+    /** @var string $secret */
+    private $secret;
+
     public function __construct(
         string $source,
-        string $cache
+        string $cache,
+        string $secret
     )
     {
        $this->source = $source;
        $this->cache = $cache;
+       $this->secret = $secret;
     }
 
     public function show(Request $request): Response
     {
-        $path = StringHelper::replaceLastMiddleDashToDot(
-            $request->attributes->get('path')
-        );
+        $path = $request->attributes->get('path');
+
+        // validate signature
+        try {
+            SignatureFactory::create($this->secret)->validateRequest($path, $request->query->all());
+        } catch (SignatureException $signatureException) {
+            throw new NotFoundHttpException($signatureException->getMessage());
+        }
 
         $server = ServerFactory::create([
             'response' => new SymfonyResponseFactory($request),
