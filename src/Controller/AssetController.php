ecea7fc1a762f53c94f816a82458d8fdc5fea05e
ludovicjj <jahanlud@gmail.com>
2020-09-25 18:27:03 +0200

    Get parameters with ParamaterBagInterface

--- src/Controller/AssetController.php
+++ src/Controller/AssetController.php
@@ -9,53 +9,53 @@ use League\Glide\Responses\SymfonyResponseFactory;
 use League\Glide\ServerFactory;
 use League\Glide\Signatures\SignatureException;
 use League\Glide\Signatures\SignatureFactory;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AssetController
 {
-    /** @var string $source */
-    private $source;
+    /** @var string $sourcePath */
+    private $sourcePath;
 
-    /** @var string $cache */
-    private $cache;
+    /** @var string $cachePath */
+    private $cachePath;
 
     /** @var string $secret */
     private $secret;
 
     public function __construct(
-        string $source,
-        string $cache,
-        string $secret
+        ParameterBagInterface $parameterBag
     )
     {
-       $this->source = $source;
-       $this->cache = $cache;
-       $this->secret = $secret;
+       $this->sourcePath = $parameterBag->get('kernel.project_dir').'/public/images';
+       $this->cachePath = $parameterBag->get('kernel.cache_dir');
+       $this->secret = $parameterBag->get('glide.key');
     }
 
-    public function show(Request $request): Response
+    public function show(Request $request, int $width, int $height, string $path): Response
     {
-        $path = $request->attributes->get('path');
+        $server = ServerFactory::create([
+            'response' => new SymfonyResponseFactory(),
+            'cache' => $this->cachePath,
+            'source' => $this->sourcePath,
+            'cache_path_prefix' => 'images',
+        ]);
+
+        [$url] = explode('?', $request->getRequestUri());
+
 
         // validate signature
         try {
-            SignatureFactory::create($this->secret)->validateRequest($path, $request->query->all());
+            SignatureFactory::create($this->secret)->validateRequest($url, ['s' => $request->query->get('s')]);
         } catch (SignatureException $signatureException) {
-            throw new NotFoundHttpException($signatureException->getMessage());
+            throw new HttpException(403, 'Signature invalid');
         }
 
-        $server = ServerFactory::create([
-            'response' => new SymfonyResponseFactory($request),
-            'cache' => $this->cache,
-            'source' => $this->source,
-            'cache_path_prefix' => '.images',
-            'base_url' => 'img'
-        ]);
-
         try {
-            $response = $server->getImageResponse($path, $request->query->all());
+            $response = $server->getImageResponse($path, ['w' => $width, 'h' => $height, 'fit' => 'crop']);
         } catch (FileNotFoundException $exception) {
             throw new NotFoundHttpException($exception->getMessage());
         }
